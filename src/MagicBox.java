41da35e31d6b4af5c2192a93e847892e11029b20
PowerOleg <geraskin.oleg@mail.ru>
2022-11-11 00:45:42 +0500

    MagicBox frame structure

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,2 +1,14 @@
-public class MagicBox {
+public class MagicBox<T> {
+    T[] items;
+    public <T> boolean add (T item) {
+
+        return true;                                //!
+    }
+    public <T> T pick(T[] items) {
+    T[] array = items;
+        return array[0];                            //!
+    }
+    public MagicBox(int number) {
+        items = (T[]) new Object[number];
+    }
 }
