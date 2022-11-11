1e4cf5f37d83a068ce7a768a63c2e95f3975ea1b
PowerOleg <geraskin.oleg@mail.ru>
2022-11-11 17:32:11 +0500

    Add method is done

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,14 +1,26 @@
-public class MagicBox<T> {
-    T[] items;
-    public <T> boolean add (T item) {
+import java.util.Arrays;
 
-        return true;                                //!
-    }
-    public <T> T pick(T[] items) {
-    T[] array = items;
-        return array[0];                            //!
-    }
+public class MagicBox<T> {
+    private T[] items;
     public MagicBox(int number) {
         items = (T[]) new Object[number];
     }
+    public boolean add(T item) {
+            for (int i = 0; i < items.length; i++) {
+                if (items[i] == null) {
+                    items[i] = item;
+                    return true;
+                }
+            }
+            System.out.println("The array is full");
+            return false;
+    }
+//    public <T> T pick(T[] items) {
+//        T[] array = items;
+//        return array[0];                            //!
+//    }
+    public T[] getItems() {
+        return items;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        MagicBox<Integer> magicBox = new MagicBox<Integer>(3);
+        magicBox.add(1);
+        magicBox.add(5);
+        magicBox.add(10);
+    }
 
 }
