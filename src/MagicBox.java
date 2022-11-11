b32f9db2f95808bf46e3fa253643e8dd7a205b61
PowerOleg <geraskin.oleg@mail.ru>
2022-11-11 19:03:03 +0500

    Pick method in process

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class MagicBox<T> {
     private T[] items;
@@ -15,10 +16,29 @@ public class MagicBox<T> {
             System.out.println("The array is full");
             return false;
     }
-//    public <T> T pick(T[] items) {
-//        T[] array = items;
-//        return array[0];                            //!
-//    }
+    public T pick(T[] items) {
+            if (items[items.length-1] != null) {
+                Random random = new Random();
+                int randomInt = random.nextInt(items.length);
+                return items[randomInt];
+            } else {
+                int vacantSlot = items.length;
+                for (int i = 0; i < items.length; i++) {
+                    if (items[i] != null) vacantSlot--;
+                }
+//                throw RuntimeException e {
+                    System.out.println("The magic box isn't full. There are " + vacantSlot + " slots left");
+
+                }
+            }
+
+
+
+
+
+        T[] array = items;
+        return array[0];                            //!
+    }
     public T[] getItems() {
         return items;
     }
