fad69547cb5956149364237c9dfe25adf56cf573
PowerOleg <geraskin.oleg@mail.ru>
2022-12-16 23:46:51 +0500

    testPick() 2.0-snapshot

--- src/test/java/ru/netology/homework26/MagicBoxTest.java
+++ src/test/java/ru/netology/homework26/MagicBoxTest.java
@@ -56,29 +56,83 @@ public class MagicBoxTest {
     }
 
 
-
+    //тест, что метод add проходит и возвращает true
     @Test
-    public <T> void testAdd() throws Exception {
-
+    public <T> void testTrueAdd() {
         final int magicBoxLength = 4;
         final Integer[] argument = {-1000, 100, 0};
-        final Integer[] expected2 = {-1000, 100, 0, null};
-
-
         MagicBox<Integer> magicBox = new MagicBox<>(magicBoxLength);
-//тест что метод add проходит возвраая true
+
         for (int i = 0; i < argument.length; i++) {
             final boolean result1 = magicBox.add(argument[i]);
             Assertions.assertTrue(result1);
         }
-//тест что метод add добавил в items нужные значения
-        final Object[] result2 = magicBox.getItems();
-
-        Assertions.assertArrayEquals(expected2, result2);
-
-//тест что метод add не добавит больше значений чем нужно
-
     }
+
+    //тест, что метод add добавил в items нужные значения
+    @Test
+    public <T> void testIntegerAdd() {
+        final int magicBoxLength = 4;
+        final Integer[] argument = {-1000, 100, 0};
+        final Integer[] expected = {-1000, 100, 0, null};
+        MagicBox<Integer> magicBox = new MagicBox<>(magicBoxLength);
+
+        for (int i = 0; i < argument.length; i++) {
+            magicBox.add(argument[i]);
+        }
+        final Object[] result2 = magicBox.getItems();
+        Assertions.assertArrayEquals(expected, result2);
+    }
+
+    //тест что метод вернет false при добавлении  элементов больше чем можно
+    @Test
+    public <T> void testLimitAdd() {
+        final int magicBoxLength = 4;
+        final Integer[] argument = {-1000, 100, 0, 100500, -6845654};
+        MagicBox<Integer> magicBox = new MagicBox<>(magicBoxLength);
+
+        for (int i = 0; i < argument.length-1; i++) {
+            magicBox.add(argument[i]);
+        }
+        Assertions.assertFalse(magicBox.add(argument[argument.length-1]));
+    }
+
+
+
+
+
+
+
+//тест что метод pick не достает элемент, если MagicBox не заполнен
+    @Test
+    public void testExceptionPick() {
+        final int magicBoxLength = 3;
+        final Integer[] argument = {0, -6845654};
+        MagicBox<Integer> magicBox = new MagicBox<>(magicBoxLength);
+        for (int i = 0; i < argument.length; i++) {
+            magicBox.add(argument[i]);
+        }
+
+        Assertions.assertThrows(RuntimeException.class, () -> magicBox.pick());
+    }
+
+
+//тест, что метод pick достает один элемент случайно
+    @Test
+    public <T> T testIntegerPick() throws RuntimeException {
+    final int magicBoxLength = 3;
+    final Integer[] argument = {0, -6845654};
+    MagicBox<Integer> magicBox = new MagicBox<>(magicBoxLength);
+    for (int i = 0; i < argument.length; i++) {
+        magicBox.add(argument[i]);
+    }
+
+    Assertions.assertThrows(RuntimeException.class, () -> magicBox.pick());
+}
+
+
+
+
 
     //        @ParameterizedTest
 //        @ValueSource(strings = { "Hello", "World" })
